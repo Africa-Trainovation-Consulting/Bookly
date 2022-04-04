72bc0e1a4e96d36d028e6d631a1225f06cc7d78b
unknown <simeonolawale69@gmail.com>
2022-04-04 17:56:58 +0100

    second form

--- script/main.js
+++ script/main.js
@@ -1,11 +1,19 @@
+// function matchpassword() {
+//   var x = document.getElementById("#validationCustom01");
+//   var y = document.getElementById("validationCustom02");
+//   if (x != y) {
+//     alert("password did not match");
+//   } else {
+//   }
+// }
 function myfunction() {
   var x = document.getElementById("nav");
   var i = document.getElementById("icon");
 
   if (x.className === "navbar") {
     x.className += " responsiveness ";
-    i.classList.remove("fa-bars");
-    i.classList.add("fa-times");
+    i.classList.toggle("fa-bars");
+    i.classList.toggle("fa-times");
   } else {
     x.className = "navbar";
     i.classList.remove("fa-times");
@@ -20,3 +28,56 @@ function myfunction() {
 //       x.className='navbar';
 //      }
 //     // console.log(x) }
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+  var x = document.getElementById("#validationCustom01");
+  var y = document.getElementById("validationCustom02");
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        else if (x != y) {
+            alert("password did not match");
+        }
+        // else {
+        //   alert ('password submitted');
+        // }
+    
+        form.classList.add("was-validated");
+        
+      },
+      false
+    );
+  });
+})();
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#validationCustom01");
+
+togglePassword.addEventListener("click", function (e) {
+  // toggle the type attribute
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  // toggle the eye slash icon
+  this.classList.toggle("fa-eye-slash");
+});
+const ogglePassword = document.querySelector("#ogglePassword");
+const assword = document.querySelector("#validationCustom02");
+
+ogglePassword.addEventListener("click", function (e) {
+  // toggle the type attribute
+  const type =
+    assword.getAttribute("type") === "password" ? "text" : "password";
+  assword.setAttribute("type", type);
+  // toggle the eye slash icon
+  this.classList.toggle("fa-eye-slash");
+});
