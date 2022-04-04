addc353ffcb738ea14a7970b4affb63f2cfb6b67
gift56 <efeasiughu@gmail.com>
2022-04-04 17:56:49 +0100

    Finished the whole section

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,1 +1,40 @@
 changeicon = (icon) => icon.classList.toggle('fa-times');
+
+const showpassword = document.getElementById('showpassword')
+showpassword.addEventListener('click', togglevisibility)
+
+function togglevisibility() {
+    const passwordinput = document.getElementById('password')
+    if (passwordinput.type === 'password') {
+        passwordinput.type = 'text'
+        showpassword.classList.toggle('fa-eye-slash')
+    }
+    else {
+        passwordinput.type = 'password'
+        showpassword.classList.toggle('fa-solid fa-eye')
+    }
+}
+
+
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
