fbdd839268cc808851e9935d0ade8c45d790f78e
unknown <simeonolawale69@gmail.com>
2022-04-26 12:07:22 +0100

    sortingfile

--- payment/order.js
+++
@@ -1,9 +0,0 @@
-function myfunction() { 
-    nav.classList.toggle("navbar"); 
-    nav.classList.toggle("responsiveness");
-    icon.classList.toggle("fa-bars");
-    icon.classList.toggle("fa-times");
-
-  };
-
-  const help = () => nvv.classList.toggle('showhelp');
